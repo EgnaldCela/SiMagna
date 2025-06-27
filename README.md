920521c74e10ce989afa3cfa4a020dbb899477f7
ppietropiccolo <pipiccolo2015@gmail.com>
2025-06-28 00:30:34 +0200

    add trained model

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 ### Where are we now
 * I put together a dataset that kinda works, the classes used are in the end of temp.ipynb, where the value is the number of bounding boxes for each label
 * some things like artichoke are not part of it because it wasn't working
-* a problem could be unbalanced classes, as you can see from the table -^
+* a problem could be unbalanced classes, as you can see from the table in temp -^
 * another problem is that there are some segments among the boxes, they should be found and removed
 * trained 11m for 1 epoch, it takes around 8 minutes, weights are in runs/detect/train/weights
+* also trained a better 11s for 72 epochs, weights are in runs/detect/from_scratch_bs32/weights
 * you should start working on the interface, just assume you have a list of ingredients found as i wrote there
 * I also think it would be nice to add ingredients, so check if you find something
 * let's try to not interfere with each others github changes, for example i will not touch the interface
@@ -24,7 +25,7 @@
 - [] object detection: (image) -> (image with labelled boxes, list of ingredients)
 - [] give the output to the recipe finder: (list of ingredients) -> (list of recipes)
 
-### Datasets
+### Datasets - useless basically
 | name | links | used | description |
 |:-------------:|:----------------:|:----------------------:|:------:|
 | initial one with fruit with or w/o bags | [kaggle]() | only one used | everything on the same background |
--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def test(model):
 		"project": "runs/detect/predict",
 		"show": False,
 		"classes": None, # quick way of choosing specific ids to predict among
-		"conf": 0.25 # minimum level of confidence before showing a box
+		"conf": 0.1 # minimum level of confidence before showing a box
 	}
-	random_images = [f"{folder}/valid/images/" + random.choice(os.listdir(f"{folder}/valid/images")) for _ in range(10)]
-	model.predict("data/fridge_example.jpg", **params)
+	# random_images = [f"{folder}/valid/images/" + random.choice(os.listdir(f"{folder}/valid/images")) for _ in range(10)]
+	# model.predict("data/fridge_example.jpg", **params)
 	model.predict("data/fridge_example2.jpg", **params)
-	model.predict("data/fridge_example3.jpg", **params)
-	model.predict("data/fruit_example.jpg", **params)
-	[model.predict(random_image, **params) for random_image in random_images]
+	# model.predict("data/fridge_example3.jpg", **params)
+	# model.predict("data/fruit_example.jpg", **params)
+	# [model.predict(random_image, **params) for random_image in random_images]
 	# result have attribute boxes, contains the list of things found: https://docs.ultralytics.com/modes/predict/#working-with-results
 
 def old_stuff():
@@ -35,13 +35,18 @@ def old_stuff():
 # # Train the model
 if __name__ == '__main__':
 
-	model = YOLO("yolo11s.pt") # COCO-pretrained YOLO model
-	# model = YOLO("runs/detect/train2/weights/best.pt") # my model
+	# model = YOLO("yolo11s.pt") # COCO-pretrained YOLO model
+	# model = YOLO("runs/detect/train_small_bs16/weights/last.pt") # my model <- use last.pt to restore lr and optimizer state <- fake it didn't work
+	model = YOLO("runs/detect/from_scratch_bs32/weights/best.pt")
 	# model.info() # Display model information (optional)
 	
 	# the best batch size for 11s is 32, takes ~ 3mins per epoch but it could perform worse, to be tested
 	# 16 workers seems a bit excessive
-	results = model.train(data = "merged2/data.yaml", epochs = 1, save_period = 5, imgsz = 640, device = 0, batch = 32, time = 1.5, name = "???") # train
+	# results = model.train(data = "merged2/data.yaml", epochs = 150, resume = False, imgsz = 640, device = 0, cache = False, batch = 32, name = "from_scratch_bs32") # train
+	# results = model.train(data = "merged2/data.yaml", epochs = 150, resume = True, save_period = 10, imgsz = 640, device = 0, batch = 16, name = "continue_small_bs16") # didn't work
 	# i should try batch = 0.8 or something -> use 80% of gpu memory
+	# model.train(resume = True) # to resume from epoch 73 to 150
+
 	# test(model)
+	metrics = model.val(plots = True)
 	
