3b72072d8d7fe62398f2a78d3af86a6c38f02a35
carolinatorba <ctorbam@gmail.com>
2025-06-29 00:33:18 +0200

    gradio

--- GRADIO.py
+++ GRADIO.py
@@ -7,11 +7,7 @@ mytheme = gr.themes.Ocean(
     neutral_hue="neutral",
 )
 
-img_bgr = cv2.imread("./INTERFACE/placeholder.jpeg")
-img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-
-
-placeholder_img = cv2.imread("./INTERFACE/placeholder.jpeg")
+placeholder_img = cv2.imread("./data/placeholder.jpeg")
 placeholder_rgb = cv2.cvtColor(placeholder_img, cv2.COLOR_BGR2RGB)
 
 def preprocess_image(img_fridge):
@@ -30,6 +26,19 @@ def find_recipes(ingredients):
     recipes = [1, 2, 3] # first 3 recipes
     return recipes
 
+suggestions = '''1. {recipe1_name}
+Ingredients: {recipe1_ingredients}
+Preparation Time: {recipe1_preparation_time}
+Cost: {recipe1_cost}
+2. {recipe2_name}
+Ingredients: {recipe2_ingredients}
+Preparation Time: {recipe2_preparation_time}
+Cost: {recipe2_cost}
+3. {recipe3_name}
+Ingredients: {recipe3_ingredients}
+Preparation Time: {recipe3_preparation_time}
+Cost: {recipe3_cost}'''
+
 def start(img_fridge):
 
     # preprocessing and ingredient recognition
@@ -41,14 +50,15 @@ def start(img_fridge):
     ingredients = recognize_ingredients(img_fridge)
 
     # find recipes on dataset based on most ingredients per recipe
-    # input: list of ingredients -> output: list of recipes IDs
+    # input: list of ingredients -> output: output string with recipes information
     recipes = find_recipes(ingredients)
 
     # once we have the recipe IDs, access dataset to find missing information for output: 
     # all ingredients needed, preparation time, cost, link to recipe
-    # or get full recipe detail and acces preparation time, cost and link woth index?
+    # or get full recipe detail and acces preparation time, cost and link with index?
 
-    return recipes # list with 3 elements
+    return suggestions
+
 
 with gr.Blocks(theme=mytheme) as demo:
     gr.Markdown("## SiMagna")
@@ -56,48 +66,19 @@ with gr.Blocks(theme=mytheme) as demo:
     with gr.Row():
         inp = gr.Image(label='Input', value=placeholder_rgb)
         with gr.Column():
-            out = gr.Text(label = 'Here you have 3 recipes you might like:', value = '''
-                            1. {recipe1_name}
-                            Ingredients: {recipe1_ingredients}
-                            Preparation Time: {recipe1_preparation_time}
-                            Cost: {recipe1_cost}
-
-                            2. {recipe2_name}
-                            Ingredients: {recipe2_ingredients}
-                            Preparation Time: {recipe2_preparation_time}
-                            Cost: {recipe2_cost}
-
-                            3. {recipe3_name}
-                            Ingredients: {recipe3_ingredients}
-                            Preparation Time: {recipe3_preparation_time}
-                            Cost: {recipe3_cost}''')   
+            out = gr.Text(label = 'Here you have 3 recipes you might like:', show_copy_button=True)   
+            
             link1 = gr.Button(value="recipe1", link="https://ricette.giallozafferano.it/Spaghetti-alla-Carbonara.html", size="sm")
             link2 = gr.Button(value="recipe 2", link="link", size="sm")
             link3 = gr.Button(value="recipe 3", link="link", size="sm")
         
-        
     with gr.Row():
                 button_cook = gr.Button("Let's cook!", variant="primary")
-                #button_cook.click(fn = start, inputs=inp, outputs=out)
-                
                 button_faster = gr.Button(value="find faster recipes", variant="primary")
-        
- 
 
-    #button_cook.click(fn = start, inputs=inp, outputs=out)
     button_cook.click(fn = start, inputs=inp, outputs=out)
+   # button_faster.click(fn = faster, inputs = inp, outputs=out)
 
-
-    '''@button_cook.click(inputs=[inp], outputs=[out])
-    def start(inp):
-        ...
-        return ...'''
-
-
-    @button_faster.click(inputs=[], outputs=[])
-    def new_recipes():
-        ...
-        return 
 
 #demo.launch()
 demo.launch(pwa=True, inbrowser=True) 
