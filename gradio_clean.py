29ee4f0180dfe2443810e204410674f1695edc87
carolinatorba <ctorbam@gmail.com>
2025-06-30 16:46:46 +0200

    updated gradio

--- gradio_clean.py
+++ gradio_clean.py
@@ -13,11 +13,6 @@ mytheme = gr.themes.Ocean(
 placeholder_img = cv2.imread("./data/placeholder.jpeg")
 placeholder_rgb = cv2.cvtColor(placeholder_img, cv2.COLOR_BGR2RGB)
 
-def preprocess_image(img_fridge):
-    # balancing ? 
-    return img_fridge
-
-
 # read ingredients dataset
 df = pd.read_excel("italian gastronomic recipes dataset/foods/CSV/FoodDataset.xlsx")
 
@@ -36,7 +31,6 @@ def find_recipes(img_fridge, df):
     return recipes_string, link1, rec1, link2, rec2, link3, rec3
 
 def start(img_fridge):
-    img_fridge = preprocess_image(img_fridge)
     ingredients = recognize_ingredients(img_fridge)
     recipes_string, l1, rec1, l2, rec2, l3, rec3 = find_recipes(ingredients)
     return recipes_string, gr.update(value=rec1, link=l1), gr.update(value=rec2, link=l2), gr.update(value=rec3, link=l3)
@@ -64,3 +58,15 @@ with gr.Blocks(theme=mytheme) as demo:
 
 #demo.launch()
 demo.launch(pwa=True, inbrowser=True, share=False) 
+
+
+'''Gradio knows what buttons to update and in what order because:
+    The order of outputs=[...] matches the order of values returned by the function.
+    Gradio expects start(...) to return:
+    1. A value for out (the gr.Text),
+    2. A .update(...) for b_link1,
+    3. A .update(...) for b_link2,
+    4. A .update(...) for b_link3.
+
+    Gradio uses positional matching — there’s no automatic linking based on variable names.
+    '''
