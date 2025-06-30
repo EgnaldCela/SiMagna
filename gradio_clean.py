5c4c8991074ed32965d2453351c5ed85c4e002a4
carolinatorba <ctorbam@gmail.com>
2025-06-30 16:53:56 +0200

    GRADIO FINISHED

--- gradio_clean.py
+++ gradio_clean.py
@@ -13,10 +13,11 @@ mytheme = gr.themes.Ocean(
 placeholder_img = cv2.imread("./data/placeholder.jpeg")
 placeholder_rgb = cv2.cvtColor(placeholder_img, cv2.COLOR_BGR2RGB)
 
-# read ingredients dataset
-df = pd.read_excel("italian gastronomic recipes dataset/foods/CSV/FoodDataset.xlsx")
 
-def find_recipes(img_fridge, df):
+def find_recipes(img_fridge):
+    # read ingredients dataset
+    df = pd.read_excel("italian gastronomic recipes dataset/foods/CSV/FoodDataset.xlsx")
+
     results = main_recipe_finder(img_fridge, df)
     recipes_string = "Hey! Based on what you have, these are some recipes you can make"
 
@@ -28,12 +29,16 @@ def find_recipes(img_fridge, df):
     rec2 = results[1]
     rec3 = results[2]
 
-    return recipes_string, link1, rec1, link2, rec2, link3, rec3
+    # return recipes_string, link1, rec1, link2, rec2, link3, rec3
+    return recipes_string, gr.update(value=rec1, link=link1), gr.update(value=rec2, link=link2), gr.update(value=rec3, link=link3)
 
+
+'''
 def start(img_fridge):
     ingredients = recognize_ingredients(img_fridge)
     recipes_string, l1, rec1, l2, rec2, l3, rec3 = find_recipes(ingredients)
     return recipes_string, gr.update(value=rec1, link=l1), gr.update(value=rec2, link=l2), gr.update(value=rec3, link=l3)
+'''
 
 with gr.Blocks(theme=mytheme) as demo:
     gr.Markdown("## SiMagna \n\nWelcome to SiMagna! We'll help you discover some new Italian recipes, and show you that you can make a masterpiece starting from any base ingredient in your fridge and a quick stop at the supermarket.\n\nPlease upload a photo of your fridge so we can suggest a recipe from ingredients you already have.", container=True)
@@ -51,7 +56,7 @@ with gr.Blocks(theme=mytheme) as demo:
         
     with gr.Row():
         button_cook = gr.Button("Let's cook!", variant="primary")
-        button_cook.click(fn = start, inputs=inp, outputs=[out, b_link1, b_link2, b_link3], show_progress='full')
+        button_cook.click(fn = find_recipes, inputs=inp, outputs=[out, b_link1, b_link2, b_link3], show_progress='full')
 
         button_faster = gr.Button("Find faster recipes", variant="primary")
 
