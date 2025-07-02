7da645221f861f2d8ae3bda5cf6acfca4f981c8e
ppietropiccolo <pipiccolo2015@gmail.com>
2025-07-02 23:32:26 +0200

    syntax changes + recipes_string handled

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def find_recipes(img_fridge):
     df = pd.read_excel("italian gastronomic recipes dataset/foods/CSV/FoodDataset.xlsx")
 
     results = main_recipe_finder(img_fridge, df)
-    recipes_string = results[6]
+    recipes_string = results[6] if len(results) >= 7 else None
     link1 = results[1]
     link2 = results[3]
     link3 = results[5]
@@ -35,14 +35,20 @@ def find_recipes(img_fridge):
     rec2 = results[2]
     rec3 = results[4]
 
-    return gr.update(label='Hey! Based on what you have, these are some recipes you can make', value=recipes_string), gr.update(value=rec1, link=link1, visible=True, interactive=True, variant='primary'), gr.update(value=rec2, link=link2, visible=True, interactive=True, variant='primary'), gr.update(value=rec3, link=link3, visible=True, interactive=True, variant='primary'), gr.update(interactive=True, variant='huggingface')
+    return (gr.update(label='Hey! Based on what you have, these are some recipes you can make', value=recipes_string),
+            gr.update(value=rec1, link=link1, visible=True, interactive=True, variant='primary'),
+            gr.update(value=rec2, link=link2, visible=True, interactive=True, variant='primary'),
+            gr.update(value=rec3, link=link3, visible=True, interactive=True, variant='primary'),
+            gr.update(interactive=True, variant='huggingface'))
 
 # Create the Gradio interface
 # Use Blocks to create a more structured layout with rows and columns
 
 with gr.Blocks(theme=mytheme) as demo:
     gr.Markdown("## SiMagna")
-    gr.Markdown("Welcome to SiMagna! We'll help you discover some new recipes from the famous Italian website Giallo Zafferano, and show you that you can make a masterpiece starting from any base ingredient in your house and a quick stop at the supermarket, while also fighting food waste.\nPlease upload a photo of your available ingredients so we can suggest some recipes.", container=False)
+    gr.Markdown("""Welcome to SiMagna! We'll help you discover some new recipes from the famous Italian website Giallo Zafferano,
+                and show you that you can make a masterpiece starting from any base ingredient in your house and a quick stop at the supermarket,
+                while also fighting food waste.\nPlease upload a photo of your available ingredients so we can suggest some recipes.""", container=False)
 
     with gr.Row():
 
@@ -54,7 +60,8 @@ with gr.Blocks(theme=mytheme) as demo:
 
             # Create output components, not visible until the main button is clicked
 
-            out = gr.Text(label = '', value='', show_copy_button=True, info="Cost indicator of the food is a discrete interval from 1 to 5 \nDifficulty indicator of the food is a discrete interval from 1 to 4 \nPreparation time of the food is expressed in minutes ")
+            out = gr.Text(label = '', value='', show_copy_button=True, info="Cost indicator of the food is a discrete interval from 1 to 5" \
+            "Difficulty indicator of the food is a discrete interval from 1 to 4 \nPreparation time of the food is expressed in minutes ")
 
             b_link1 = gr.Button(value="recipe 1", link='link1', size="sm", visible=False, variant='secondary', interactive=False)
             b_link2 = gr.Button(value="recipe 2", link='link2', size="sm", visible=False, variant='secondary', interactive=False)
@@ -71,7 +78,12 @@ with gr.Blocks(theme=mytheme) as demo:
         # For the clear button bring the input and output components to their original state of non visibility or non interactivity
 
         button_cook.click(fn = find_recipes, inputs=inp, outputs=[out, b_link1, b_link2, b_link3, clear], show_progress='full')
-        clear.click(fn=lambda: [gr.update(value=placeholder_rgb), gr.update(value="recipe 1", link='link1', size="sm", visible=False, variant='secondary', interactive=False), gr.update(value="recipe 2", link='link2', size="sm", visible=False, variant='secondary', interactive=False), gr.update(value="recipe 3", link='link3', size="sm", visible=False, variant='secondary', interactive=False), gr.update(variant="secondary", visible=True, interactive=False), gr.update(label = '')], outputs=[inp, b_link1, b_link2, b_link3, clear, out])
+        clear.click(fn=lambda: [gr.update(value=placeholder_rgb),
+                                gr.update(value="recipe 1", link='link1', size="sm", visible=False, variant='secondary', interactive=False),
+                                gr.update(value="recipe 2", link='link2', size="sm", visible=False, variant='secondary', interactive=False),
+                                gr.update(value="recipe 3", link='link3', size="sm", visible=False, variant='secondary', interactive=False),
+                                gr.update(variant="secondary", visible=True, interactive=False), gr.update(label = '')],
+                                outputs=[inp, b_link1, b_link2, b_link3, clear, out])
 
 # launch demo with link available for sharing for easy use also on the mobile phone, tablet, computer
 demo.launch(pwa=False, inbrowser=True, share=True) 
